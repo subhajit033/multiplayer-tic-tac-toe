cf721d45978b3ffb5ea4882530d63e9951af4e17
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-12-07 12:37:25 +0530

    ready for uploading on render

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
 
 const app = express();
 const server = createServer(app);
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 const io = new Server(server, {
   cors: {
@@ -19,9 +26,15 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+
+const clientDir = path.join(__dirname, 'client', 'dist');
+
+app.use(express.static(clientDir));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientDir, 'index.html'));
 });
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
